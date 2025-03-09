2c2a2ae65d6175794df44a8a1d64bfda8138c1e7
pokhiii <pokhi@coloredcow.com>
2025-03-09 14:39:06 +0530

    use breeze-next code

--- src/app/signin/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignInForm from "@/components/sections/Auth/SignInForm";
-
-export default function SignInPage() {
-  return (
-    <main className="flex items-center justify-center min-h-screen">
-      <SignInForm />
-    </main>
-  );
-}
--- src/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "@/components/sections/Auth/SignUpForm";
-
-export default function SignUpPage() {
-  return (
-    <main className="flex items-center justify-center min-h-screen">
-      <SignUpForm />
-    </main>
-  );
-}
